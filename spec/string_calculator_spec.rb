1a9c580f79a6528c74b7832745380c6de4f1e2ad
tanisha <tanishadevmind@gmail.com>
2024-03-12 16:15:10 +0530

    Added specs

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,27 @@
 require 'rspec'
 require './lib/string_calculator'
 RSpec.describe StringCalculator do
+  context 'with valid input' do
+    it 'returns the sum of numbers' do
+      expect(described_class.new.add("1,2,3")).to eq("6")
+    end
+
+    it 'handles negative numbers' do
+      expect(described_class.new.add("-1,2,3,-4")).to eq("Negative numbers not allowed: [-1, -4]")
+    end
+
+    it 'handles new line delimiter' do
+      expect(described_class.new.add("1\n2\n3")).to eq("6")
+    end
+
+    it 'handles custom delimiter' do
+      expect(described_class.new.add("//;\n1;2;3")).to eq("6")
+    end
+  end
+
+  context 'with invalid input' do
+    it 'handles invalid input format' do
+      expect(described_class.new.add("1,\n2")).to eq("Invalid input: '1,\n2'")
+    end
+  end
 end
