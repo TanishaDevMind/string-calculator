7d26911addda8c3c299687e7d75b2a537d9a6392
tanisha <tanishadevmind@gmail.com>
2024-03-12 16:12:46 +0530

    Following changes added 1. Directory structure 2. Required gem 3. Required classes 4. Required spec

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,4 @@
+require 'rspec'
+require './lib/string_calculator'
+RSpec.describe StringCalculator do
+end
---
+++ starter.rb
@@ -0,0 +1,2 @@
+require_relative 'lib/string_calculator'
+StringCalculator.new.add("", "1", "1,5", "1\n2,3","1,\n","//;\n1;2", "//;\n1;2,-2")
